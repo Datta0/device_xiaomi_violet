0ee6ad6e5520f948ccf664d0ac93f12abf706f6e
Dyneteve <sound0020@gmail.com>
2019-07-21 19:43:48 +0300

    violet: Build libaudioroute

--- device.mk
+++ device.mk
@@ -76,6 +76,11 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
+    libaudioroute \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libvolumelistener \
     libaudio-resampler \
     tinymix
 
