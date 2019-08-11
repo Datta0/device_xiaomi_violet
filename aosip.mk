9b2763d15a0c11a6d974054c9208f6737eb7e16e
rashedsahaji <asifrashed2@gmail.com>
2019-08-11 17:05:00 +0530

    violet: derpfest: Build with Gapps
    Signed-off-by: rashedsahaji <asifrashed2@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -14,8 +14,15 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common PixelExperience stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+
+# Gapps
+$(call inherit-product-if-exists, vendor/gapps/config.mk)
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# PixelStyle
+$(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosip_violet
